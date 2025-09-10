bff2f92fca8cd8755e375ae6171b91b2ed75aab8
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-09-10 06:20:47 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,60 @@
-print("hello")
+import turtle
+t=turtle
+t.shape('turtle')
+"""def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+def rectangle(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x+250)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x+250)
+    t.left(90)
+rectangle(200)"""
+for i in range(3):
+    print(i)
+    sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90)
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+addSquares(60)
