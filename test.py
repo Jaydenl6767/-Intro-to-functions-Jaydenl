69ce66c5696881ff5b69a7bb1eafcc5ca5a7e9ff
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-09-16 06:15:00 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import turtle
 t=turtle
 t.shape('turtle')
+t.speed(100)
 """def square(x):
     t.forward(x)
     t.left(90)
@@ -37,33 +38,48 @@ def rectangle(x):
     t.forward(x+250)
     t.left(90)
 rectangle(200)"""
-for i in range(3):
+"""for i in range(3):
     print(i)
-    sidelength = 100
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+    sidelength = 100"""
 rotate = 90
-t.speed(600)
-def square (x,y):
+def square(x,y):
     for i in range(4):
         t.forward(x)
         t.left(y)
-"""square(100,90)
-def doubleSquares(iRange):
+"""def doubleSquares(iRange):
     length = 25
-for i in range(iRange):
+    for i in range(iRange):
         square(length, 90)
         length = length * 2
-        length += 25
-doubleSquares(100)
-def addSquares(iRange):"""
-    length = 25:
-    addSquares(60)
+doubleSquares(5)"""
 def addSquares(iRange):
     length = 25
     for i in range(iRange):
         square(length, 90)
-        length += 25
-   
-addSquares(60)
+        length += 5
+        t.left(5)
+addSquares(600)
+
+def star(x,y):
+    for i in range(5):
+        t.forward(x)
+        t.left(y)
+    def doublestars(irange):
+        t.right(5)
+        star(length, 144)
+        length = length + 5
+    doublestars(6000)
+
+
+
+turtle.done()
+
+
+
+
    
 
 
